6385ebf98f9733b59e3872b4bdca623b61e8fb1b
Christian Koopmann <c.k.e.koopmann@gmail.com>
2020-04-19 16:52:38 +0200

    Milestone 4 working

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -1,4 +1,5 @@
 package observatory
+import Visualization._
 
 /**
   * 4th milestone: value-added information
@@ -11,7 +12,12 @@ object Manipulation extends ManipulationInterface {
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    var tempMap =  Map[GridLocation, Temperature]()
+    for(lat <- -89 to 90;
+        lon <- -180 to 179){
+        tempMap = tempMap.updated(GridLocation(lat, lon), predictTemperature(temperatures, Location(lat.toFloat, lon.toFloat)))
+        }
+    tempMap
   }
 
   /**
@@ -20,7 +26,12 @@ object Manipulation extends ManipulationInterface {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+    val grids = temperaturess.map(makeGrid)
+    def averageFun(loc: GridLocation): Temperature = {
+      val temps = grids.map(_.apply(loc))
+      temps.sum / temps.size
+    }
+    averageFun
   }
 
   /**
@@ -29,7 +40,11 @@ object Manipulation extends ManipulationInterface {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+    val grid = makeGrid(temperatures)
+    def averageFun(loc: GridLocation): Temperature = {
+      grid(loc) - normals(loc)
+    }
+    averageFun
   }
 
 
